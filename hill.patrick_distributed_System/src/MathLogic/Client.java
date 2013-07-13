fba238a956a78c2ff6567d3dc93a7a312293a153
ArmyHill01 <phill@student.neumont.edu>
2013-07-13 11:43:00 -0600

    Added functionality for Part 2 of the assignment
    Server now uses Reflection to populate Math.java's methods available for
    the client to request.

--- hill.patrick_distributed_System/src/MathLogic/Client.java
+++ hill.patrick_distributed_System/src/MathLogic/Client.java
@@ -11,43 +11,65 @@ import java.util.Scanner;
  * Time: 9:03 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class Client {
 
     public static void main(String[] args) {
-        String request;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Add or Sub (Subtract)?");
-        request = s.nextLine();
-        System.out.println("Enter your first integer...");
-        request += "-" + s.nextInt();
-        System.out.println("Enter your second integer...");
-        request += "," + s.nextInt();
+        new ClientSide();
+    }
 
-        // Connect to server and send the request
+    public static class ClientSide {
+        Socket s;
+        Scanner scan = new Scanner(System.in);
 
-        try {
-            Socket socket = new Socket("localhost", 8080);
-            OutputStream sOut = socket.getOutputStream();
-            InputStream sIn = socket.getInputStream();
+        public ClientSide() {
+            connectToServer();
+        }
 
-            PrintWriter sWrite = new PrintWriter(sOut, true);
-            sWrite.println(request);
-            sWrite.flush();
-            socket.shutdownOutput();
+        private void connectToServer() {
+            try {
+                System.out.println("Connecting to the server...");
+                s = new Socket("localhost", 8080);
+                OutputStream sOut = s.getOutputStream();
+                InputStream sIn = s.getInputStream();
+                PrintWriter sWrite = new PrintWriter(sOut, true);
+                BufferedReader br;
+                br = new BufferedReader( new InputStreamReader(sIn));
+                System.out.println("Connected to server and Requesting the server's methods...");
+                sWrite.println("methods");
+                sWrite.flush();
 
-            BufferedReader br;
-            br = new BufferedReader( new InputStreamReader(sIn));
+                String methodList = br.readLine();
+                System.out.println("Received Method List");
+                String methods[] = methodList.split(", ");
 
-            String serverResponse = br.readLine();
+                System.out.println("Server replied with: ");
+                int count2 = 0;
+                for (String str : methods) {
+                    System.out.println("#"+count2+": "+str);
+                    count2++;
+                }
+                System.out.println("Enter the # corresponding to the method you would like to invoke");
+                String userPick = scan.nextLine();
+                sWrite.println(userPick);
+                sWrite.flush();
 
-//            Scanner s2 = new Scanner(sIn);
-//            String serverResponse = s2.nextLine();
-            System.out.println("MathServer Replied: " + serverResponse);
-            socket.shutdownInput();
-            socket.close();
+                System.out.println("Enter your parameters comma separated as per your method choice");
+                System.out.println("i.e.: 4,5,6 or 200,3000,8 \n");
+                String userParams = scan.nextLine();
+                sWrite.println(userParams);
+                sWrite.flush();
 
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                // read final answer from the server
+                String answer = br.readLine();
+                System.out.println("MathServer Replied: " + answer);
+                s.shutdownInput();
+                s.shutdownOutput();
+                s.close();
+                scan.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
