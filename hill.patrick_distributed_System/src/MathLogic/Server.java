78a27fe27c1efc110cfe21257d6066e720e4234d
ArmyHill01 <phill@student.neumont.edu>
2013-07-15 23:48:54 -0600

    DS Part 3 Implementation added as inline class inside Server.java
    Replace MathServer with an inline class inside Server.java.
    
    Much more work is needed....

--- hill.patrick_distributed_System/src/MathLogic/Server.java
+++ hill.patrick_distributed_System/src/MathLogic/Server.java
@@ -1,7 +1,10 @@
 package MathLogic;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * User: Patrick
@@ -23,6 +26,87 @@ public class Server {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    protected class ObjectServer implements Runnable {
+        private Socket s;
+        private OutputStream sOut;
+        private InputStream sIn;
+        private Class c = null;
+
+        private String clientClass;
+        private Method[] classMethods;
+
+        public ObjectServer(Socket s, int connectionCount) {
+            this.s = s;
+            System.out.println("Server #: "+connectionCount + " has been started.");
+        }
+
+        @Override
+        public void run() {
+            startConnection();
+        }
+
+        public void startConnection() {
+            try {
+                sOut = s.getOutputStream();
+                BufferedReader br;
+                br = new BufferedReader( new InputStreamReader( s.getInputStream()));
+                PrintWriter serverWrite = new PrintWriter(sOut, true);
+                Class c;
+                clientClass = br.readLine();
+                c = getInstance();
+                // Received class name
+                Object con = c.getConstructor().newInstance();
+                classMethods = c.getDeclaredMethods();
+                System.out.println("Sending Methods...");
+                String methodList = "";
+                for(Method m : classMethods) {
+                    methodList += m + ", ";
+                }
+                serverWrite.println(methodList);
+                serverWrite.flush();
+
+                System.out.println("Waiting for client response...");
+                int methodRequest = Integer.parseInt(br.readLine());
+                Method m = classMethods[methodRequest];
+
+                String clientParams;
+                clientParams = br.readLine(); // schema param1, param2
+                String toMethods[] = clientParams.split(",");
+                // Convert String to int's
+                int intPs[] = new int[toMethods.length];
+                for (int i=0;i<toMethods.length;i++)
+                    intPs[i] = Integer.parseInt(toMethods[i]);
+                // Invoke proper method
+                Method m2 = c.getDeclaredMethod(m.getName(),
+                        m.getParameterTypes());
+                Object answer = m2.invoke(con, intPs);
+                serverWrite.println(answer.toString());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+        private Class getInstance() throws ClassNotFoundException,
+                IllegalAccessException, InstantiationException {
+            if(c == null) {
+                c = Class.forName(clientClass);
+                c.newInstance();
+            }
+            return c;
         }
     }
 }
