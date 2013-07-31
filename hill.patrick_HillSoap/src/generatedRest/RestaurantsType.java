4a975c419f4ce159c925bda1c687e1b9f71cb385
ArmyHill01 <phill@student.neumont.edu>
2013-07-30 21:45:21 -0600

    HillSoap
    Adding missing file

---
+++ hill.patrick_HillSoap/src/generatedRest/RestaurantsType.java
@@ -0,0 +1,67 @@
+
+package generatedRest;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for restaurantsType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="restaurantsType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="restaurant" type="{}restaurantType" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "restaurantsType", propOrder = {
+    "restaurant"
+})
+public class RestaurantsType {
+
+    protected List<RestaurantType> restaurant;
+
+    /**
+     * Gets the value of the restaurant property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the restaurant property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getRestaurant().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link RestaurantType }
+     * 
+     * 
+     */
+    public List<RestaurantType> getRestaurant() {
+        if (restaurant == null) {
+            restaurant = new ArrayList<RestaurantType>();
+        }
+        return this.restaurant;
+    }
+
+}
